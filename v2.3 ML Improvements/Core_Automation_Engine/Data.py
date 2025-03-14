771ad3f841137c411e99ae721b01d2540a084028
Soham Jadhav <soham.ai.engineer@gmail.com>
2025-03-14 23:33:07 +0530

    Improved Prediction Visual

---
+++ v2.3 ML Improvements/Core_Automation_Engine/Data.py
@@ -0,0 +1,28 @@
+import pandas as pd
+data = r"E:\My Space\Data\XAU\1 Min\XAU_1_MIn_1_Jan_to_11_Mar_(10_Ema)_1_2_RR_processed.csv"
+
+def Data_rows():
+    try:
+        dataset = pd.read_csv(data)
+        first_100_rows = dataset.head(100)
+        last_100_rows = dataset.tail(100)
+        return first_100_rows, last_100_rows
+    except Exception as e:
+        print(f"An error occurred in Data_rows: {e}")
+        return None, None
+
+def filepath():
+    data_path = data
+    return data_path
+        
+def dataset_features():
+    data_path = filepath()
+    dataset = pd.read_csv(data_path)
+    
+    dataset_features = {
+        "shape": dataset.shape,
+        "size": dataset.size,
+        "columns": dataset.columns,
+        "dtypes": dataset.dtypes
+    }
+    return dataset_features
